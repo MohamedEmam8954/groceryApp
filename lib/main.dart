39254a3821dca1a17e6981421588ae1778d46234
Mohamed emam <145616277+MohamedEmam8954@users.noreply.github.com>
2024-10-21 14:21:08 +0300

    add firebase to our project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,9 +6,13 @@ import 'package:grocery/app/groceryapp.dart';
 import 'package:grocery/core/cache/my_cache_helper.dart';
 import 'package:grocery/core/models/my_blocobserver.dart';
 import 'package:grocery/core/utils/dependencyInjection.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // Ensure Flutter is initialized
   init();
   Bloc.observer = MyBlocObserver();
   await getIt.get<MyCacheHelper>().sharePerferences();
